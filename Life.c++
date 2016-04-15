48e6a769f9a6572d621a7693258079d1990219ce
InsaneBuildEnvironment <samuel.b.lauber@gmail.com>
2016-04-15 17:03:14 -0500

    Compile errors

--- Life.c++
+++ Life.c++
@@ -3,3 +3,40 @@
 
 #include "Life.h"
 
+using namespace std;
+
+ConwayCell::ConwayCell(const char& input) {
+	if (input == '*')
+		alive = true;
+	else
+		alive = false;
+}
+
+Cell ConwayCell::evolve(vector<Cell> neighbors) {
+	int live_neighbors = 0;
+
+	for (Cell n : neighbors) {
+		AbstractCell* nc = n.cell;
+		if (nc->alive)
+			live_neighbors++;
+	}
+
+	if (alive) {
+		if (live_neighbors < 2 || live_neighbors > 3)
+			return Cell(new ConwayCell('.'));
+		else
+			return Cell(new ConwayCell('*'));
+	} else { // dead
+		if (live_neighbors == 3)
+			return Cell(new ConwayCell('*'));
+		else
+			return Cell(new ConwayCell('.'));
+	}
+}
+
+void ConwayCell::print(ostream& out) {
+	if (alive)
+		out << '*';
+	else
+		out << '.';
+}
--- Life.h
+++ Life.h
@@ -4,29 +4,28 @@
 #include <vector>
 #include <iostream>
 
-class AbstractCell {
-	friend virtual ostream& operator<<(ostream& lhs, const AbstractCell& rhs);
+class Cell;
 
+class AbstractCell {
 public:
-	virtual Cell evolve(vector<Cell> neighbors);
+	virtual Cell evolve(std::vector<Cell> neighbors);
+	virtual void print(std::ostream& out);
 protected:
 	bool alive;
 };
 
-class ConwayCell : protected AbstractCell {
-	friend ostream& operator<<(ostream& lhs, const ConwayCell& rhs);
-
+class ConwayCell : public AbstractCell {
 public:
 	ConwayCell(const char& input);
-	Cell evolve(vector<Cell> neighbors);
+	void print(std::ostream& out);
+	Cell evolve(std::vector<Cell> neighbors);
 };
 
-class FredkinCell : protected AbstractCell {
-	friend ostream& operator<<(ostream& lhs, const FredkinCell& rhs);
-
+class FredkinCell : public AbstractCell {
 public:
 	FredkinCell(const char& input);
-	Cell evolve(vector<Cell> neighbors);
+	void print(std::ostream& out);
+	Cell evolve(std::vector<Cell> neighbors);
 private:
 	int age;
 };
@@ -35,18 +34,18 @@ class Cell {
 public:
 	AbstractCell *cell;
 
-	Cell(AbstractCell& c);
-	Cell(Cell& c);
+	Cell(AbstractCell* c) : cell(c) {}
+	//Cell(Cell& c);
 	~Cell();
 	Cell& operator=(const Cell& rhs);
 };
 
 template <typename T>
-class Life<T> {
-	friend ostream& operator<<(ostream& lhs, const Life<T> rhs);
-
+class Life {
 public:
-	Life(istream& board);
+	Life(std::istream& board);
+
+	void print(std::ostream& out);
 
 	void evolve_all();
 	Cell& at(int x, int y);
@@ -56,7 +55,7 @@ public:
 private:
 	int height;
 	int width;
-	vector<Cell> board;
+	std::vector<Cell> board;
 
 	int generation;
 	int population;
